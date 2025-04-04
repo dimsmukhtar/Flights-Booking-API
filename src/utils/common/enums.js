ed72899e30e18c4e562ac3392150a7d598fe26f6
dimas <dimsmukhtar@gmail.com>
2025-04-04 22:34:20 +0700

    added booking model

--- src/utils/common/enums.js
+++ src/utils/common/enums.js
@@ -5,4 +5,11 @@ const SEAT_TYPE = {
   FIRST_CLASS: "first-class",
 }
 
-module.exports = { SEAT_TYPE }
+const BOOKING_STATUS = {
+  BOOKED: "booked",
+  CANCELLED: "cancelled",
+  INITIATED: "initiated",
+  PENDING: "pending",
+}
+
+module.exports = { SEAT_TYPE, BOOKING_STATUS }
